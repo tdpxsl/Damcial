3e28aa43a261e03a94830eb5e41e94926a9b1d61
tdpxsl <116475197+tdpxsl@users.noreply.github.com>
2022-10-23 07:15:18 +0100

    Damcial

---
+++ index.php
@@ -0,0 +1,122 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="google-site-verification" content="+nxGUDJ4QpAZ5l9Bsjdi102tLVC21AIh5d1Nl23908vVuFHs34=">
+    <meta name="robots" content="noindex,nofollow">
+    <meta name="google" content="nopagereadaloud">
+    <meta name="google" content="nositelinkssearchbox">
+    <meta name='language' content='EN'>
+    <meta name='copyright' content='Damcial'>
+    <meta name='Classification' content='Social Media'>
+    <meta name="googlebot" content="notranslate">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Damcial Is A Website For Anyone To Talk With Everyone.">
+    <meta name="keywords" content="DAMCIAL,damc-al,social,media">
+    <meta name='subject' content='Social Media Website'>
+    <meta name='owner' content='Adam Farouk'>
+    <meta name='url' content='https://www.damcial.com'>
+    <meta name='identifier-URL' content='https://www.damcial.com'>
+    <meta name='target' content='all'>
+    <meta name='og:title' content='Damcial'>
+    <meta name='og:url' content='https://www.damcial.com'>
+    <meta name='og:site_name' content='Damcial'>
+    <meta name='og:description' content='Damcial Is A Website For Anyone To Talk With Everyone.'>
+    <meta name='og:email' content='damcial@gmail.com'>
+    <meta name="author" content="Adam Farouk, thwanpo@gmail.com">
+    <meta property="og:type" content="website">
+    <title>Damcial</title>
+    <style>
+           <?php include 'style.css';?>
+    </style>
+    <script src="https://kit.fontawesome.com/14914739aa.js" crossorigin="anonymous"></script>
+    <script>
+    function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}</script>
+    <noscript>Your browser does not support JavaScript!</noscript>
+</head>
+<body>
+<header>
+      <a href="index.html" class="website-logo">
+        <span class="big-title-letter">D</span>
+        <span class="normal-letters">amsocial</span>
+      </a>
+      <ul class="list-type">
+        <li class="list">
+          <span>
+            <a href="#" class="list-item">About us</a>
+          </span>
+        </li>
+        <li class="list">
+          <span>
+            <a href="#" class="list-item">FAQ</a>
+          </span>
+        </li>
+        <li class="list">
+          <span>
+            <a href="#" class="list-item">Contact</a>
+          </span>
+        </li>
+      </ul>
+      <ul class="login-registeration">
+        <li class="login-link">
+          <span>
+            <a href="#" class="profile-info">
+              <i class="fa-solid fa-right-to-bracket"></i>
+              &nbsp;&nbsp;login
+            </a>
+          </span>
+        </li>
+        <li class="registeration-link">
+          <span>
+            <a href="#" class="profile-info">
+              <i class="fa-solid fa-user-plus"></i>
+              &nbsp;&nbsp;Register
+            </a>
+          </span>
+        </li>
+      </ul>
+    </header>
+    <main>
+      <aside>
+        <img
+          src="phone.jpg"
+          alt="A man chatting with his friend"
+          class="phone-image"
+        />
+      </aside>
+      <label class="switch">
+        <input type="checkbox" onclick="myFunction()" />
+        <span class="slider round"></span>
+      </label>
+      <center>
+        <div class="about-website">
+          <h1 class="title-header">
+            <div class="start-span">
+              <div class="chat-with">Chat With</div>
+              <div class="your-friends">Your Friend Using</div>
+            </div>
+            <span class="title-span">Damsocial</span>
+          </h1>
+        </div>
+        <div class="something">
+          <h1 class="secured">
+            <i class="fa fa-check"></i>
+            &nbsp;Your Informations Are Secured.
+          </h1>
+          <h1 class="data-sell">
+            <i class="fa fa-check"></i>
+            &nbsp;We Do Not Sell Your Personal Data.
+          </h1>
+          <h1 class="report-check">
+            <i class="fa fa-check"></i>
+            &nbsp;We Completely Check Any Report In The Website.
+          </h1>
+        </div>
+      </center>
+    </main>
+</body>
+</html>
